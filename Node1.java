c78d57d56019cc985165b266e2724cdc7ed95558
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-04-20 17:31:21 +0530

    Node class is created

---
+++ Node1.java
@@ -0,0 +1,24 @@
+class Node1{
+    int data;
+    Node1 next;
+    Node1(int data){
+        this.data = data;
+        next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node1 getNext() {
+        return next;
+    }
+
+    public void setNext(Node1 next) {
+        this.next = next;
+    }
+}
